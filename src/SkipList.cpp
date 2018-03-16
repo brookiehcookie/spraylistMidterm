0ddec0e708e51848eba9f7fb454c66005acf96b3
Harrison Geiger <hcgeiger@knights.ucf.edu>
2018-03-16 19:02:09 -0400

    Add AtomicMarkableReference class

--- src/SkipList.cpp
+++ src/SkipList.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <cmath>
+#include <atomic>
 
 // Really quick down-and dirty boolean
 #define bool int
@@ -11,6 +12,100 @@
 #define maybe (rand()%2)
 
 using namespace std;
+
+/**************************************************/
+
+/**
+	Simulate Java's AtomicMarkableReference
+
+	This uses the least significant bit of a pointer
+	as the mark. This creates a restriction on the kinds
+	of pointers you can use, but most of the time 
+	it should be fine.
+
+	References:
+	http://www.cnblogs.com/zxh1210603696/p/3721460.html
+	https://stackoverflow.com/questions/40247249/what-is-the-c-11-atomic-library-equivalent-of-javas-atomicmarkablereferencet
+*/
+
+template <class T>
+class AtomicMarkableReference {
+private:
+	std::atomic<T> ref;
+	const uintptr_t UNMARKED = 0;
+	const uintptr_t MARKED = 1;
+public:
+	AtomicMarkableReference(T p, bool mark);
+	AtomicMarkableReference(const AtomicMarkableReference &a);
+	operator = (const AtomicMarkableReference &a);
+	T getRef() const;
+	bool isMarked() const;
+	bool get(bool &b) const;
+	bool compareAndSet(T expectedValue, T newValue, bool expectedMark, bool newMark);
+	void set(T newValue, bool newMark);
+	bool attemptMark(T expectedValue, bool newMark);
+};
+
+template <class T>
+AtomicMarkableReference<T>::AtomicMarkableReference(T p, bool mark) {
+	this->ref.store((T) ((uintptr_t) p | (uintptr_t) mark));
+}
+
+template <class T>
+AtomicMarkableReference<T>::AtomicMarkableReference(const AtomicMarkableReference &a) {
+	this->ref.store(a.ref.load());
+}
+
+template <class T>
+AtomicMarkableReference<T>::operator = (const AtomicMarkableReference &a) {
+	if (*this != a) {
+		this->ref.store(a.ref.load());
+		return *this;
+	}
+}
+
+template <class T>
+T AtomicMarkableReference<T>::getRef() const {
+	T p = this->ref.load();
+	return (bool) ((uintptr_t) p & MARKED) ? (T)((uintptr_t)(p) & ~MARKED) : p;
+}
+
+template <class T>
+bool AtomicMarkableReference<T>::isMarked() const {
+	T p = this->ref.load();
+	return (bool) ((uintptr_t) p & MARKED);
+}
+
+template <class T>
+bool AtomicMarkableReference<T>::get(bool &b) const {
+	T p = this->ref.load();
+	b = (bool) ((uintptr_t) p & MARKED);
+	return b ? (T) ((uintptr_t)(p) & ~MARKED) : p;
+}
+
+template <class T>
+bool AtomicMarkableReference<T>::compareAndSet(T expectedValue, T newValue, bool expectedMark, bool newMark) {
+	T p = this->ref.load();
+	bool b = (bool)((uintptr_t) p & MARKED);
+	if (b == expectedMark) {
+		expectedValue = (T)((uintptr_t) expectedValue | (uintptr_t) b);
+		return this->ref.compare_exchange_strong(expectedValue, (T) ((uintptr_t) newValue | (uintptr_t) newMark));
+	}
+	return false;
+}
+
+template <class T>
+void AtomicMarkableReference<T>::set(T newValue, bool newMark) {
+	newValue = (T) ((uintptr_t) newValue | (uintptr_t) newMark);
+	this->ref.exchange(newValue);
+}
+
+template <class T>
+bool AtomicMarkableReference<T>::attemptMark(T expectedValue, bool newMark) {
+	T newValue = (T) ((uintptr_t) expectedValue | (uintptr_t) newMark);
+	expectedValue = isMarked() ? (T) ((uintptr_t) expectedValue | MARKED) : expectedValue;
+	return this->ref.compare_exchange_strong(expectedValue, newValue);
+}
 
 /**************************************************/
 
@@ -260,6 +355,7 @@ void SkipList<T>::print() {
 	// TODO: write (lock-free) SkipList print
 }
 
+
 /**************************************************/
 
 /**
@@ -272,5 +368,7 @@ int main() {
 	skippy.add(1);
 
 	cout << "Done" << endl;
+
 	return 0;
 }
+
