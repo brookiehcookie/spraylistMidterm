009923c54ec05604bb180233790e5f9a58251a77
Brooke\Brooke C <fibrobr@gmail.com>
2018-03-15 23:12:20 -0400

    added pseudocode for add

--- src/sprayList.cpp
+++ src/sprayList.cpp
@@ -1,4 +1,51 @@
 
+
+template <class T>
+bool SkipList::remove(T x)
+{
+  int bottomLevel = 0;
+  Node<T>[] preds = (Node<T>[]) new Node [MAX_LEVEL +1]; // c++ equiv need
+  Node<T>[] succs = (Node<T>[]) new Node [MAX_LEVEL +1]; // C++ equiv need
+  Node<T> succ;
+  
+  while(true)
+  {
+    bool found = find(x,preds, succs); // need find method
+    if (!found)
+      return false;
+    else
+    {
+      Node<T>  nodeToRemove = succ[bottomLevel]; // need c++ equiv
+      for(int level = nodeToRemove.topLevel; level >= bottomLevel+1; level--)
+      {
+        bool[] marked = (false);
+        succ = nodeToRemove.next[level].get(marked); // get c++ equiv
+        while(!marked[0])
+        {
+          nodeToRemove.next[level].compareAndSet(succ, succ, false, true); // need C++ equiv
+          succ = nodeToRemove.next[level].get(marked);
+        }
+      }
+      bool[] marked = {false};
+      succ = nodeToRemove.next[bottomLevel].get(marked); // need C++ equiv
+      
+      while(true)
+      {
+        boolean iMarkedIt = nodeToRemove.next[bottomLevel.compareAndSet(succ, succ, false, true); // Need C++ Equiv
+        succ = siccs[bottomLevel].compareAndSet(succ, succ, false, true);
+        if(iMarketIt)
+        {
+          find(x,preds,succs); // need find method
+          return true;
+        }
+        else if(marked[0])
+          return false;
+      }
+    }
+  }
+ }
+}
+
 template <class T>
 bool SkipList::add(T x) 
 {
